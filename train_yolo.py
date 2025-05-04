67adae2d3682c0620824d7c3518580cd833d37e1
liambarstad <liambarstad@gmail.com>
2025-05-04 02:15:45 +0000

    train control yolo

--- train_yolo.py
+++ train_yolo.py
@@ -5,6 +5,12 @@ from ultralytics import YOLO
 
 parser = argparse.ArgumentParser(description='train yolo model')
 parser.add_argument(
+    '--data_dir',
+    type=str,
+    required=True,
+    help='The absolute path to the data to be used and modified'
+)
+parser.add_argument(
     '--experiment_name',
     type=str,
     required=True,
@@ -14,15 +20,41 @@ parser.add_argument(
 args = parser.parse_args()
 
 
+def log_metrics(run_dir: str):
+    pass
+
 def train():
+    yolo = YOLO('yolo12n.pt')
+
+    config = {
+        'epochs': 100,
+        'batch_size': 64,
+        'imgsz': 640,
+        'patience': 5
+    }
+
     wandb.init(
         project='BYU-locating-flagellar-motors',
         name=args.experiment_name,
-        config={}
+        config=config
     )
 
-    yolo = YOLO('yolo12n.pt')
+    yolo_weights_dir = os.path.join(args.data_dir, 'output', 'yolo-weights')
 
+    results = yolo.train(
+        data=os.path.join(os.getcwd(), 'yolo-config.yaml'),
+        project=yolo_weights_dir,
+        name='motor_detector',
+        exist_ok=True,
+        save_period=5,
+        val=True,
+        verbose=True,
+        device='cuda'
+    )
+
+    # Get the path to the run directory
+    log_metrics(os.path.join(yolo_weights_dir, 'motor_detector'))
+    
 
 if __name__ == '__main__':
     train()
